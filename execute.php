933950b8c121f71583d360d6c0528a6a7b04228a
Nabil Khalaf <nabil.khalaf.pro@gmail.com>
2017-03-09 10:01:54 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,40 +1,29 @@
 <?php
-
 // Authentification
-
 require_once '_include/connection.php';
-
 $sql = 'SELECT *
         FROM `functions`
         WHERE id = ?';
-
 $r = $db->prepare($sql);
 $r->execute(array($_GET['function_id']));
-
 if ($r->rowCount() != 1) {
   exit('Fonction introuvable.');
 }
-
 $function = $r->fetch();
-
 echo $function['code'];
-
-
-
-
 // Maintenant, on va executer le code qu'on peut retrouver dans la variable ici : $function['code']
-
-
 $start_time = microtime(TRUE);
+
 $result = eval($function['code']);
+
 $end_time = microtime(TRUE);
 
 $total_time = $end_time - $start_time;
+
 $total_time = round($total_time, 4, PHP_ROUND_HALF_UP);
-
-
 ?>
 
+// exemple : return $_GET['integer'] + 1 ;
 
 <p>
   La fonction vient de se terminer en <?php echo $total_time; ?> secondes (avec le résultat : <?php echo $result ?>).
